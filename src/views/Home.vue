f9b7f08c9aea244a4be6eba0912c85a9b442f5be
Andrés M. Jiménez <andresjimenezrios@gmail.com>
2019-03-31 13:33:08 +0200

    Added info in the index page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,169 +1,168 @@
 <template>
-  <div>
-    <section>
-      <v-parallax :src="require('../assets/newspaper.jpg')" height="600">
-        <v-layout
-          column
-          align-center
-          justify-center
-          class="white--text"
-        >
-          <img src="../assets/icon.png" alt="Vuetify.js" height="200">
-          <h1 class="white--text mb-2 display-1 text-xs-center">Parallax Template</h1>
-          <div class="subheading mb-3 text-xs-center">Powered by Vuetify</div>
-          <v-btn
-            class="mt-5"
-            color="accent"
-            dark
-            large
-            href="/pre-made-themes"
-          >
-            Get Started
-          </v-btn>
-        </v-layout>
-      </v-parallax>
-    </section>
+    <v-flex>
+        <section>
+            <v-parallax :src="require('../assets/newspaper.jpg')" height="600">
+                <v-layout
+                        column
+                        align-center
+                        justify-center
+                        class="white--text"
+                >
+                    <img src="../assets/icon.png" alt="Vuetify.js" height="200">
+                    <h1 class="white--text mt-2 mb-2 display-1 text-xs-center">LT-News</h1>
+                    <div class="subheading mb-3 text-xs-center">Another option to read the digital newspaper</div>
+                    <v-btn
+                            class="mt-5"
+                            color="accent"
+                            dark
+                            large
+                            :to="{name: 'ItemList'}"
+                    >
+                        Get Started
+                    </v-btn>
+                </v-layout>
+            </v-parallax>
+        </section>
 
-    <section>
-      <v-layout
-        column
-        wrap
-        class="my-5"
-        align-center
-      >
-        <v-flex xs12 sm4 class="my-3">
-          <div class="text-xs-center">
-            <h2 class="headline">The best way to start developing</h2>
-            <span class="subheading">
-                Cras facilisis mi vitae nunc
+        <section>
+            <v-layout
+                    column
+                    wrap
+                    class="my-5"
+                    align-center
+            >
+                <v-flex xs12 sm4 class="my-3">
+                    <div class="text-xs-center">
+                        <h2 class="headline">Why LT-News?</h2>
+                        <span class="subheading">
+                It arises from the need to be informed, but the huge amount of resources and the few time is a real problem.
               </span>
-          </div>
-        </v-flex>
-        <v-flex xs12>
-          <v-container grid-list-xl>
-            <v-layout row wrap align-center>
-              <v-flex xs12 md4>
-                <v-card class="elevation-0 transparent">
-                  <v-card-text class="text-xs-center">
-                    <v-icon x-large color="primary">color_lens</v-icon>
-                  </v-card-text>
-                  <v-card-title primary-title class="layout justify-center">
-                    <div class="headline text-xs-center">Material Design</div>
-                  </v-card-title>
-                  <v-card-text>
-                    Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                    Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
-                  </v-card-text>
-                </v-card>
-              </v-flex>
-              <v-flex xs12 md4>
-                <v-card class="elevation-0 transparent">
-                  <v-card-text class="text-xs-center">
-                    <v-icon x-large color="primary">flash_on</v-icon>
-                  </v-card-text>
-                  <v-card-title primary-title class="layout justify-center">
-                    <div class="headline">Fast development</div>
-                  </v-card-title>
-                  <v-card-text>
-                    Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                    Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
-                  </v-card-text>
-                </v-card>
-              </v-flex>
-              <v-flex xs12 md4>
-                <v-card class="elevation-0 transparent">
-                  <v-card-text class="text-xs-center">
-                    <v-icon x-large color="primary">build</v-icon>
-                  </v-card-text>
-                  <v-card-title primary-title class="layout justify-center">
-                    <div class="headline text-xs-center">Completely Open Sourced</div>
-                  </v-card-title>
-                  <v-card-text>
-                    Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                    Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
-                  </v-card-text>
-                </v-card>
-              </v-flex>
+                    </div>
+                </v-flex>
+                <v-flex xs12>
+                    <v-container grid-list-xl>
+                        <v-layout row wrap align-center>
+                            <v-flex xs12 md4>
+                                <v-card class="elevation-0 transparent">
+                                    <v-card-text class="text-xs-center">
+                                        <v-icon x-large color="primary">rss_feed</v-icon>
+                                    </v-card-text>
+                                    <v-card-title primary-title class="layout justify-center">
+                                        <div class="headline text-xs-center">Read news</div>
+                                    </v-card-title>
+                                    <v-card-text>
+                                        LT-News is, definitely, a news' reader from the newspapers o blogs that you follow.
+                                        In this site, you will follow some online newspaper or blog. How? Through its RSS syndication.
+                                    </v-card-text>
+                                </v-card>
+                            </v-flex>
+                            <v-flex xs12 md4>
+                                <v-card class="elevation-0 transparent">
+                                    <v-card-text class="text-xs-center">
+                                        <v-icon x-large color="primary">group_work</v-icon>
+                                    </v-card-text>
+                                    <v-card-title primary-title class="layout justify-center">
+                                        <div class="headline">Group news</div>
+                                    </v-card-title>
+                                    <v-card-text>
+                                        LT-News group related news. Thank to this, you will find out what other newspapers says about the same news.
+                                        You can also follow the evolution of a new.
+                                    </v-card-text>
+                                </v-card>
+                            </v-flex>
+                            <v-flex xs12 md4>
+                                <v-card class="elevation-0 transparent">
+                                    <v-card-text class="text-xs-center">
+                                        <v-icon x-large color="primary">group</v-icon>
+                                    </v-card-text>
+                                    <v-card-title primary-title class="layout justify-center">
+                                        <div class="headline text-xs-center">Made for you</div>
+                                    </v-card-title>
+                                    <v-card-text>
+                                        As you using LT-News, it will make you knowing more and more.
+                                        It will recommend news based on your interests.
+                                        So, you will never be lost among infinity news.
+                                    </v-card-text>
+                                </v-card>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </v-flex>
             </v-layout>
-          </v-container>
-        </v-flex>
-      </v-layout>
-    </section>
+        </section>
 
-    <section>
-      <v-parallax :src="require('../assets/internet.jpg')" height="380">
-        <v-layout column align-center justify-center>
-          <div class="headline white--text mb-3 text-xs-center">Web development has never been easier</div>
-          <em>Kick-start your application today</em>
-          <v-btn
-            class="mt-5"
-            color="accent"
-            dark
-            large
-            href="/pre-made-themes"
-          >
-            Get Started
-          </v-btn>
-        </v-layout>
-      </v-parallax>
-    </section>
+        <section>
+            <v-parallax :src="require('../assets/internet.jpg')" height="380">
+                <v-layout column align-center justify-center>
+                    <div class="headline white--text mb-3 text-xs-center">Newspaper union</div>
+                    <em>When you log in, you will view the newspaper or blogs that you follow.</em>
+                    <v-btn
+                            class="mt-5"
+                            color="accent"
+                            dark
+                            large
+                            :to="{name: 'SectionList'}"
+                    >
+                        Get Started
+                    </v-btn>
+                </v-layout>
+            </v-parallax>
+        </section>
 
-    <section>
-      <v-container grid-list-xl>
-        <v-layout row wrap justify-center class="my-5">
-          <v-flex xs12 sm4>
-            <v-card class="elevation-0 transparent">
-              <v-card-title primary-title class="layout justify-center">
-                <div class="headline">Company info</div>
-              </v-card-title>
-              <v-card-text>
-                Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
-              </v-card-text>
-            </v-card>
-          </v-flex>
-          <v-flex xs12 sm4 offset-sm1>
-            <v-card class="elevation-0 transparent">
-              <v-card-title primary-title class="layout justify-center">
-                <div class="headline">Contact us</div>
-              </v-card-title>
-              <v-card-text>
-                Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-              </v-card-text>
-              <v-list class="transparent">
-                <v-list-tile>
-                  <v-list-tile-action>
-                    <v-icon color="accent">phone</v-icon>
-                  </v-list-tile-action>
-                  <v-list-tile-content>
-                    <v-list-tile-title>777-867-5309</v-list-tile-title>
-                  </v-list-tile-content>
-                </v-list-tile>
-                <v-list-tile>
-                  <v-list-tile-action>
-                    <v-icon color="accent">place</v-icon>
-                  </v-list-tile-action>
-                  <v-list-tile-content>
-                    <v-list-tile-title>Chicago, US</v-list-tile-title>
-                  </v-list-tile-content>
-                </v-list-tile>
-                <v-list-tile>
-                  <v-list-tile-action>
-                    <v-icon color="accent">email</v-icon>
-                  </v-list-tile-action>
-                  <v-list-tile-content>
-                    <v-list-tile-title>john@vuetifyjs.com</v-list-tile-title>
-                  </v-list-tile-content>
-                </v-list-tile>
-              </v-list>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </section>
-  </div>
+        <section>
+            <v-container grid-list-xl>
+                <v-layout row wrap justify-center class="my-5">
+                    <v-flex xs12 sm4>
+                        <v-card class="elevation-0 transparent">
+                            <v-card-title primary-title class="layout justify-center">
+                                <div class="headline">Company info</div>
+                            </v-card-title>
+                            <v-card-text>
+                                LT-News learns of your form of reading news: it detects your interests and it recommends
+                                you news that may interest you. It converts the digital press in a new world for you.
+                                It is not yet the typical newspaper (large and bored); it is web designed for you
+                                complety. It is possible thanks a recommends tools and machine learning...
+                            </v-card-text>
+                        </v-card>
+                    </v-flex>
+                    <v-flex xs12 sm4 offset-sm1>
+                        <v-card class="elevation-0 transparent">
+                            <v-card-title primary-title class="layout justify-center">
+                                <div class="headline">Contact us</div>
+                            </v-card-title>
+                            <v-card-text>
+                                Converting the sea of daily news into a glass for each, that allows it to be token at the time the user wants.
+                            </v-card-text>
+                            <v-list class="transparent">
+                                <v-list-tile>
+                                    <v-list-tile-action>
+                                        <v-icon color="accent">person</v-icon>
+                                    </v-list-tile-action>
+                                    <v-list-tile-content>
+                                        <v-list-tile-title>Andrés Jiménez</v-list-tile-title>
+                                    </v-list-tile-content>
+                                </v-list-tile>
+                                <v-list-tile>
+                                    <v-list-tile-action>
+                                        <v-icon color="accent">place</v-icon>
+                                    </v-list-tile-action>
+                                    <v-list-tile-content>
+                                        <v-list-tile-title>Seville, Spain</v-list-tile-title>
+                                    </v-list-tile-content>
+                                </v-list-tile>
+                                <v-list-tile>
+                                    <v-list-tile-action>
+                                        <v-icon color="accent">email</v-icon>
+                                    </v-list-tile-action>
+                                    <v-list-tile-content>
+                                        <v-list-tile-title>lt.news.app@gmail.com</v-list-tile-title>
+                                    </v-list-tile-content>
+                                </v-list-tile>
+                            </v-list>
+                        </v-card>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </section>
+    </v-flex>
 </template>
