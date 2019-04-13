e9d90b40a8c6219441cf1eb53507dbda760c4e2f
Andrés M. Jiménez <andresjimenezrios@gmail.com>
2019-04-13 12:14:25 +0200

    Dockerize node application

--- src/api/session.js
+++ src/api/session.js
@@ -6,7 +6,7 @@ const CSRF_HEADER_NAME = 'X-CSRFToken'
 const session = axios.create({
   xsrfCookieName: CSRF_COOKIE_NAME,
   xsrfHeaderName: CSRF_HEADER_NAME,
-  baseURL: 'http://127.0.0.1:8000/'
+  baseURL: 'http://localhost:8000/'
 })
 
 export default session
