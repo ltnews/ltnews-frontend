5eb7659aa8dcd6d889a7e88ec386e568547367b6
Andrés M. Jiménez <andresjimenezrios@gmail.com>
2019-04-14 11:54:16 +0200

    Add environment variables.

--- src/api/session.js
+++ src/api/session.js
@@ -3,10 +3,12 @@ import axios from 'axios'
 const CSRF_COOKIE_NAME = 'csrftoken'
 const CSRF_HEADER_NAME = 'X-CSRFToken'
 
+const HOST_IP = process.env.NODE_ENV == 'production' ? 'ltnews.cf' : 'localhost';
+
 const session = axios.create({
   xsrfCookieName: CSRF_COOKIE_NAME,
   xsrfHeaderName: CSRF_HEADER_NAME,
-  baseURL: 'http://localhost:8000/'
+  baseURL: 'http://' + HOST_IP + ':8000/'
 })
 
 export default session
